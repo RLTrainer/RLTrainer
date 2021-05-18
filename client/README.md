5cda0b5bea8a4ff28b375dfe91162f1fc24ba5c2
RLTrainer <RocketLeagueTrainerMNGMT@gmail.com>
2021-05-18 23:41:54 +0300

    Wiped old mess. Refactored and split front and back end in 2 folders. Setup basic back and front ends

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# rltrainer
+# client
 
 ## Project setup
 ```
---
+++ client/src/components/BaseComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>RLTrainer FRONTEND</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseComponent'
+}
+</script>
+
+<style scoped>
+</style>
