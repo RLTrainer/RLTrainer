bbdd87b57846fbbf0c48e51d8fd26cd89e723629
RLTrainer <RocketLeagueTrainerMNGMT@gmail.com>
2021-05-20 01:19:18 +0300

    80% of navigation, fonts and a few assets

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <NavigationComponent />
+  <div id="app">
+    <NavigationComponent navbar_style="padding-left: 4px; background-color: #292929"/>
+    <b-img class="bg" :src="require('./assets/backgrounds/homepage-1.png')"></b-img>
+    <b-img class="bg" :src="require('./assets/backgrounds/homepage-2.png')"></b-img>
+    <b-img class="bg" :src="require('./assets/backgrounds/homepage-3.png')"></b-img>
+  </div>
 </template>
 
 <script>
-import NavigationComponent from './components/NavigationComponent.vue'
+import Vue from 'vue'
+import NavigationComponent from './components/NavigationComponent'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 export default {
-  name: 'App',
   components: {
     NavigationComponent
   }
@@ -14,12 +32,38 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Navine";
+  src: local("Navine"), url(./assets/fonts/NavineDemo.ttf) format("truetype");
 }
+@font-face {
+  font-family: "QuarcaCond";
+  src: local("QuarcaCond"), url(/assets/fonts/QuarcaCond.otf) format("truetype");
+}
+
+ .bg {
+   filter: brightness(75%);
+   width: 100%;
+ }
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #292929;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #121212;
+}
+
 </style>
--- client/src/components/NavigationComponent.vue
+++ client/src/components/NavigationComponent.vue
@@ -1,41 +1,133 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar w/ text</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#"
-              >Home <span class="sr-only">(current)</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-        </ul>
-        <span class="navbar-text"> Navbar text with an inline element </span>
-      </div>
-    </nav>
+    <b-navbar id="b-navbar" :style="navbar_style" type="dark">
+      <a href="#"><b-img :src="require('../assets/RL-TRAINER.png')" height="100em"></b-img></a>
+      <div id="split"></div>
+
+      <b-navbar-nav id="b-navbar-nav-1">
+        <b-nav-item class="b-navbar-nav-1-item" href="#"
+          >Training packs</b-nav-item
+        >
+        <div class="dropdown-content">
+          <a href="#">Bronze</a>
+          <a href="#">Silver</a>
+          <a href="#">Gold</a>
+          <a href="#">Platinum</a>
+          <a href="#">Diamond</a>
+          <a href="#">Champion</a>
+          <a href="#">Grand champion</a>
+        </div>
+        <div class="split"></div>
+        <b-nav-item class="b-navbar-nav-1-item" href="#"
+          >Workshop Maps</b-nav-item
+        >
+        <b-nav-item class="b-navbar-nav-1-item" href="#">Credit</b-nav-item>
+        <b-nav-item class="b-navbar-nav-1-item" href="#">About</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <form>
+            <input placeholder="Search" type="text" id="search" />
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style scope>
+.b-navbar-nav-1-item {
+  margin-right: 2.6em;
+  font-size: 2.3em;
+  letter-spacing: 1.6px;
+  font-family: "Navine", Arial;
+  margin-top: 0.35em;
+  text-transform: uppercase;
+}
 
-<style scoped>
+.b-navbar-nav-1-item:hover {
+  border-bottom: 1px solid white;
+}
+
+.b-navbar-nav-1-item:first-child {
+  margin-left: 1.3em;
+}
+
+font-awesome-icon {
+  padding-left: 2em;
+}
+
+.split {
+  margin-right: 0.2em;
+}
+
+.b-navbar-nav-1-item:hover + .dropdown-content {
+  cursor: pointer;
+  display: block;
+}
+
+.dropdown-content:hover {
+  cursor: pointer;
+  display: block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #292929;
+  min-width: 19.1em;
+  margin-left: 3em;
+  margin-top: 5.5em;
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: #8E9494;
+  padding: 12px 16px;
+  font-family: "Navine", Arial;
+  font-size: 1.7em;
+  text-decoration: none;
+  display: block;
+  text-transform: uppercase;
+}
+
+.dropdown-content a:hover {
+  color: white;
+  text-decoration: none;
+  font-size: 1.8em;
+}
+
+#search {
+  background-color: transparent;
+  border: 0;
+  outline: 0;
+  border-bottom: 2px solid #8E9494;
+  font-family: "Navine", Arial;
+  font-size: 1.4em;
+  width: 16em;
+  height: 3em;
+  color: white;
+}
+#search:focus {
+  border-color:white;
+}
 </style>
+
+<script>
+export default {
+  props: {
+    navbar_style: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
